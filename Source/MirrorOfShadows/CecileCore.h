6b334fef76caf493278e405df1b5dd23e28ea6cf
Rhaque20 <72638964+Rhaque20@users.noreply.github.com>
2024-03-18 14:33:41 -0400

    Starting development of Cecile

---
+++ Source/MirrorOfShadows/CecileCore.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CombatComponent.h"
+#include "CecileCore.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MIRROROFSHADOWS_API UCecileCore : public UCombatComponent
+{
+	GENERATED_BODY()
+	
+};
