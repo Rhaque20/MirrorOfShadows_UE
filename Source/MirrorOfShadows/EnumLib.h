238632905eaa853bbd1a22149d75d4b56a2efd0b
Rhaque20 <72638964+Rhaque20@users.noreply.github.com>
2024-06-04 03:17:35 -0400

    Starting basis of combat system with damage and stats.

---
+++ Source/MirrorOfShadows/EnumLib.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EnumLib.generated.h"
+
+UENUM(BlueprintType)
+enum class EElement
+{
+	Physical UMETA(DisplayName = "Physical"),
+    Fire UMETA(DisplayName = "Fire"),
+    Wind UMETA(DisplayName = "Wind"),
+    Earth UMETA(DisplayName = "Earth"),
+    Water UMETA(DisplayName = "Water"),
+    Light UMETA(DisplayName = "Light"),
+    Dark UMETA(DisplayName = "Dark"),
+    Pure UMETA(DisplayName = "Pure")
+};
+
+UENUM(BlueprintType)
+enum class EStats
+{
+	Health UMETA(DisplayName = "Health"),
+    Attack UMETA(DisplayName = "Attack"),
+    Defense UMETA(DisplayName = "Defense")
+};
