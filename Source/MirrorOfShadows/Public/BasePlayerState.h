d1a3460c4126ede20747ce34a820768e3a52656e
Rhaque20 <72638964+Rhaque20@users.noreply.github.com>
2024-07-15 02:51:22 -0400

    Added in player state and moved slow mo logic to there.

---
+++ Source/MirrorOfShadows/Public/BasePlayerState.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "BasePlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MIRROROFSHADOWS_API ABasePlayerState : public APlayerState
+{
+	GENERATED_BODY()
+	public:
+		UFUNCTION(BlueprintImplementableEvent,BlueprintCallable)
+		void CancelSlowMotion();// Used for counter edge or incase some game over condition is met.
+	protected:
+		UPROPERTY(BlueprintReadWrite, EditAnywhere)
+		class APlayerCharacters* ActiveCharacter;
+
+		UPROPERTY(BlueprintReadWrite, EditAnywhere)
+		bool inCounterWindow = false;
+};
