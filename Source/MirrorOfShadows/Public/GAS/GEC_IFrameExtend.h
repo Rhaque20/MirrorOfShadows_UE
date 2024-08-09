629cb6e68ffeb70b2b93c626e54c9616465f29ae
Rhaque20 <72638964+Rhaque20@users.noreply.github.com>
2024-08-09 15:13:41 -0400

    Allowed counter edge to activate immediately. Starting new way to extend iframe but not yet made.

---
+++ Source/MirrorOfShadows/Public/GAS/GEC_IFrameExtend.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayEffectExecutionCalculation.h"
+#include "GEC_IFrameExtend.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MIRROROFSHADOWS_API UGEC_IFrameExtend : public UGameplayEffectExecutionCalculation
+{
+	GENERATED_BODY()
+	
+};
